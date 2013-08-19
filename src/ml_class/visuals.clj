ecb3d508cb2aca69dee8eafae10054771b77346f
maugrim <marshall@quander.me>
2013-08-18 17:04:28 -0700

    Added Incanter plotting for cost convergence

---
+++ src/ml_class/visuals.clj
@@ -0,0 +1,12 @@
+(ns ml-class.visuals
+  (:require [ml-class.linear-regression :as lnr]
+            [ml-class.logistic-regression :as lgr]
+            [ml-class.gradient-descent :as gd]
+            [ml-class.vector :as v])
+  (:use [incanter core stats charts]))
+
+(defn cost-plot [cost-fn vectors]
+  (xy-plot (range 0 (count vectors)) (map cost-fn vectors)
+           :title "Cost over time"
+           :x-label "Iterations"
+           :y-label "Cost"))
